9f9b1c60c50604895e5728db7967727e05a2913e
Albán <albanaguilar1@gmail.com>
2016-10-16 01:09:53 -0500

    Competitive Programming

---
+++ 2694 - More Difficult to Solve/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string sX = "This is my first solution in programming competitions";
+    cout << sX << endl;
+    return 0;
+}
